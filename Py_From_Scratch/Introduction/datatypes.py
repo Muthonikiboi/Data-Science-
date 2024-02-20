2b4a8c225f82abe1b7fbcf2d2f90eeafb661e028
Joy Muthoni <kiboijoye254@gmail.com>
2024-02-20 12:53:12 +0300

    ADD:Constants and Datatypes

---
+++ Py_From_Scratch/Introduction/datatypes.py
@@ -0,0 +1,28 @@
+# Numeric DataTypes
+num1 = 5
+print(num1, 'is of type', type(num1))
+
+num2 = 5.8
+print(num2, 'is of type', type(num2))
+
+num3 = 1+2j
+print(num3, 'is of type', type(num3))
+
+# Data List
+languages = ["Swift", "Java", "Python"]
+print(languages[0])
+print(languages[2])
+
+# Dictionary Type
+student_id = {112, 123, 234, 235, 678}
+print(student_id)
+print(type(student_id))
+
+# Access Dictionary Values
+country = {'Kenya': 'Nairobi', 'Tanzania': 'Dar-es-alam', 'Somalia': 'Mogadishu'}
+print(country['Kenya'])
+''' print(country['Nairobi'])  # This Throws an error'''
+
+
+print("Hello world")
+
--- Py_From_Scratch/Introduction/intro.py
+++ Py_From_Scratch/Introduction/intro.py
@@ -1,1 +1,22 @@
-print("hello there from the terminal in the ubuntu environment")
+import constant
+# Identifiers in python
+language = 'Python'
+print(language)
+
+# Declare a variable
+name = 'Joy Elizabeth Muthoni Kiboi'
+print(name)
+
+# Changing the value of a valuable in python
+name = 'Zaki_Dev'
+print(name)
+
+# Assigning multiple values to multiple variables
+a, b, c = 5, 'Hello World', 2.9
+print(a, b, c)
+
+# CONSTANTS
+print(constant.PI)
+print(constant.GRAVITY)
+
+# DATATYPES
