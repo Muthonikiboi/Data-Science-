01f99a0d1f7f4152688dcc5d60ffc685fcb242e6
Joy Muthoni <kiboijoye254@gmail.com>
2024-02-23 22:43:12 +0300

    ADD:For loops

---
+++ Py_From_Scratch/Flow Control/for_loop.py
@@ -0,0 +1,14 @@
+# lists
+schools = ["Dekut", "UON", "MMU", "Chuka"]
+# iterate though the list
+for i in schools:
+    print(i)
+
+# Iterating through a string
+school = "Dedan Kimathi University of Technology"
+for x in school:
+    print(x)
+
+# Using Range # output os from 0-99
+for t in range(100):
+    print(t)
