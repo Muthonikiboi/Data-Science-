b7e9f35cb68e37be65835ef7515381e1567a53e4
Muthonikiboi <kiboijoye254@gmail.com>
2025-02-18 21:35:02 +0300

    ADD:Dictionary

---
+++ Py_From_Scratch/3: Lists and Dictionaries/dictionaries.py
@@ -0,0 +1,22 @@
+capitals = {
+  "Germany": "Berlin",
+  "Canada": "Ottawa",
+  "England": "London"
+}
+print(capitals)
+
+#Accessing a dictionary item
+print(capitals["Germany"])
+
+#Adding an item to a Dictionary
+capitals["Ghana"] = "Accra"
+print(capitals)
+
+#Remove Items from a dictionary
+del capitals["England"]
+print(capitals)
+
+#Change details of a dictionary
+capitals["Canada"] = "Switzerland"
+print(capitals)
+
